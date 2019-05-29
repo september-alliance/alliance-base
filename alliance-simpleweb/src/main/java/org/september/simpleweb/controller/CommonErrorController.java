46c22277afc32a75ef269e978e1a6f424eb826bc
Administrator <Administrator@ht-PC>
2019-05-29 18:44:08 +0800

    增加404页面处理

---
+++ alliance-simpleweb/src/main/java/org/september/simpleweb/controller/CommonErrorController.java
@@ -0,0 +1,31 @@
+package org.september.simpleweb.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CommonErrorController implements ErrorController {
+
+	@RequestMapping("/error")
+    public String handleError(HttpServletRequest request){
+        //获取statusCode:401,404,500
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if(statusCode == 401){
+            return "/401";
+        }else if(statusCode == 404){
+            return "/404";
+        }else if(statusCode == 403){
+            return "/403";
+        }else{
+            return "/500";
+        }
+
+    }
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
