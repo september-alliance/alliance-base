d8b0b8d5ad2cc836469255659021aa4deeaf58bd
Administrator <Administrator@ht-PC>
2019-06-24 20:08:06 +0800

    添加时间格式

--- alliance-core/src/main/java/org/september/core/constant/DateFormatConst.java
+++ alliance-core/src/main/java/org/september/core/constant/DateFormatConst.java
@@ -9,4 +9,6 @@ public interface DateFormatConst {
 	public final static SimpleDateFormat yyyyMMdd = new SimpleDateFormat("yyyyMMdd");
 	public final static SimpleDateFormat yyyy_MM_dd_HH_mm_ss = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	public final static SimpleDateFormat yyyyMMddHHmmss = new SimpleDateFormat("yyyyMMddHHmmss");
+	
+	public final static SimpleDateFormat yyyy_MM_dd_Zn_Ch = new SimpleDateFormat("yyyy年MM月dd日");
 }
