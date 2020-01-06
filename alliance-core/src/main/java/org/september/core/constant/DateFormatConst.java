d8685ec0d607a050c4c6c2cb6838cc883968c595
yexinzhou <253187898@qq.com>
2020-01-06 11:02:54 +0800

    增加时间格式化格式

--- alliance-core/src/main/java/org/september/core/constant/DateFormatConst.java
+++ alliance-core/src/main/java/org/september/core/constant/DateFormatConst.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 public interface DateFormatConst {
 
 	public final static SimpleDateFormat yyyy = new SimpleDateFormat("yyyy");
+	public final static SimpleDateFormat yyyy_MM = new SimpleDateFormat("yyyy-MM");
 	public final static SimpleDateFormat yyyy_MM_dd = new SimpleDateFormat("yyyy-MM-dd");
 	public final static SimpleDateFormat yyyyMMdd = new SimpleDateFormat("yyyyMMdd");
 	public final static SimpleDateFormat yyyy_MM_dd_HH_mm_ss = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
