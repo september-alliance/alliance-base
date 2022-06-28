3253ea065bdff53258a0745d983c6609a6e2d8da
runningship <253187898@qq.com>
2022-06-28 10:02:55 +0800

    增加时间格式

--- alliance-core/src/main/java/org/september/core/constant/DateFormatConst.java
+++ alliance-core/src/main/java/org/september/core/constant/DateFormatConst.java
@@ -8,6 +8,7 @@ public interface DateFormatConst {
 	public final static SimpleDateFormat yyyy_MM = new SimpleDateFormat("yyyy-MM");
 	public final static SimpleDateFormat yyyy_MM_dd = new SimpleDateFormat("yyyy-MM-dd");
 	public final static SimpleDateFormat yyyyMMdd = new SimpleDateFormat("yyyyMMdd");
+	public final static SimpleDateFormat HH_mm_ss = new SimpleDateFormat("HH:mm:ss");
 	public final static SimpleDateFormat yyyy_MM_dd_HH_mm_ss = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	public final static SimpleDateFormat yyyyMMddHHmmss = new SimpleDateFormat("yyyyMMddHHmmss");
 	
